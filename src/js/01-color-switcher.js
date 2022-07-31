8649b2845eee750bc6c82f562ecf1311ff4ccf22
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-07-31 22:52:19 +0300

    task-01-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  btnStart: document.querySelector(`[data-start]`),
+  btnStop: document.querySelector(`[data-stop]`),
+  body: document.querySelector(`body`),
+};
 
+refs.btnStart.addEventListener(`click`, changeBgColor);
+refs.btnStop.addEventListener(`click`, stopChangeColor);
+let intervalId;
+
+function changeBgColor() {
+  let color = getRandomHexColor();
+  refs.body.style.backgroundColor = `${color}`;
+  refs.btnStart.setAttribute(`disabled`, true);
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function changeColor() {
+  let color = getRandomHexColor();
+  refs.body.style.backgroundColor = `${color}`;
+}
+
+function stopChangeColor() {
+  clearInterval(intervalId);
+  refs.btnStart.removeAttribute(`disabled`);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
