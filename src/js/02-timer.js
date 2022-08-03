a7ff123010bd112140a43910ac1372bb40b74df4
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-08-03 12:04:18 +0300

    add-time-convert

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -34,9 +34,28 @@ function onStart() {
 
 function delta() {
   ms = deadLine.getTime() - new Date().getTime();
-  if (ms >= 0) {
-    console.log(ms);
-  } else {
+  if (ms < 0) {
     clearInterval(id);
-  }
+  } 
+  
+  console.log(convertMs(ms));
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
