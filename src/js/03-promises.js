299668eaaba33a5e6d91c46e33450154d24720d2
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-08-03 13:48:19 +0300

    add-refs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,31 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  firstDelay: document.querySelector(`input[name="delay"]`),
+delayStep: document.querySelector(`input[name="step"]`),
+  amount: document.querySelector(`input[name="amount"]`),
+btn: document.querySelector(`button`),
+}
+
+refs.btn.addEventListener(`click`, createPromise)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
